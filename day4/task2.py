7490d01e9dabe9f6dba0e69a7ae96e0ea2d92379
Raj Rathore <rajrathore15755@gmail.com>
2026-06-16 15:49:56 +0530

    3rd commit

--- day4/task2.py
+++ day4/task2.py
@@ -1,12 +1,15 @@
-def fib(n):
-    if n<=0:
-        return n
-    elif n==1:
-        return 1
-    v =0
-    for i in range(n):
-        
-        v= v+i
-    return v
-for i in range(8):
-    print(fib(i))
+# def fib(n):
+#     if n<=0:
+#         return n
+#     elif n==1:
+#         return 1
+#     v =0
+#     for i in range(n):
+#         v= v+i
+#     return v
+# for i in range(8):
+#     print(fib(i))
+
+print("hello")
+print()  
+print("world")
